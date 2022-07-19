9b452cfed5e6d6037395c8cb37d87809dbb428da
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-07-19 14:34:56 +0800

    wrong one_away

---
+++ One Away.py
@@ -0,0 +1,29 @@
+test_cases = [
+    ('pale', 'ple', True),
+    ('pales', 'pale', True),
+    ('pale', 'bale', True),
+    ('pale', 'bake', False),
+    ('aaa', 'aab', True),
+    ('aaa', 'aaaa', True)
+]
+
+def one_away(s1, s2):
+    if abs(len(s1) - len(s2)) > 1:
+        return False
+    
+    # remove and insert
+    elif abs(len(s1) - len(s2)) == 1:
+        if abs(len(set(s1)) - len(set(s2))) <= 1:
+            return True
+    
+    # replace
+    elif abs(len(s1) - len(s2)) == 0:
+        # same string and a classic one away string
+        if abs(len(set(s1)) - len(set(s2))) in [0, 2]:
+            return True
+
+if __name__ == '__main__':
+    for test in test_cases:
+        assert one_away(test[0], test[1]) == test[2], f'{test}'
+    
+    print('all done')
