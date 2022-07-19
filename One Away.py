92cdf2e5c35c4b48744aeddec9d5264dcae569f5
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-07-19 14:48:42 +0800

    Update One Away.py

--- One Away.py
+++ One Away.py
@@ -4,23 +4,25 @@ test_cases = [
     ('pale', 'bale', True),
     ('pale', 'bake', False),
     ('aaa', 'aab', True),
-    ('aaa', 'aaaa', True)
+    ('aaa', 'aaaa', True),
+    ('abcd', 'bcda', False)
 ]
 
 def one_away(s1, s2):
+    overlapped_len = len( set(s1) & set(s2) )
+
     if abs(len(s1) - len(s2)) > 1:
         return False
-    
+
     # remove and insert
     elif abs(len(s1) - len(s2)) == 1:
-        overlapped_len = len( set(s1) & set(s2) )
         if abs( len(set(s1)) - overlapped_len ) <= 1:
             return True
     
     # replace
     elif abs(len(s1) - len(s2)) == 0:
         # same string and a classic one away string
-        if abs(len(set(s1)) - len(set(s2))) in [0, 2]:
+        if abs( len(set(s1)) - overlapped_len ) <= 1:
             return True
 
 if __name__ == '__main__':
