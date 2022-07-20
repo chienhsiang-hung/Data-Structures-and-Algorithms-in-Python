1e867c74f60321dbbdf322353d25631f87da961b
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-07-20 14:40:54 +0800

    Update One Away.py

--- Arrays and Strings/One Away.py
+++ Arrays and Strings/One Away.py
@@ -9,6 +9,9 @@ test_cases = [
 ]
 
 def one_away(s1, s2):
+    pass
+            
+def one_away_Wrong(s1, s2):
     overlapped_len = len( set(s1) & set(s2) )
 
     if abs(len(s1) - len(s2)) > 1:
@@ -24,6 +27,24 @@ def one_away(s1, s2):
         # same string and a classic one away string
         if abs( len(set(s1)) - overlapped_len ) <= 1:
             return True
+
+def one_away_Wrong1(s1, s2):
+    '''
+    Assume two strings are different in default
+    '''
+    if len( len(s1) - len(s2) ) > 1 or s1==s2:
+        return False
+    
+    else:
+        check_start = False
+        for i in range( min(len(s1), len(s2)) ):
+            
+            if check_start:
+                # remove
+                pass
+
+            if s1[i] != s2[i]:
+                check_start = True
 
 if __name__ == '__main__':
     for test in test_cases:
